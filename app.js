9553d6d2369c9279430d93821a480b808c2e7548
Suraj Raj Ranabhat <147985897+SRB-09@users.noreply.github.com>
2024-06-04 12:32:59 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+document.querySelector(".edit").addEventListener("click", function(){
+    document.querySelector(".toggle").style.display = (document.querySelector(".toggle").style.display === "none" ? "block" : "none");
+});
